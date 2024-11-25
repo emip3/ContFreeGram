635a0c8184d3a776c5bc707abae4b42e49c8dc05
DaWaildHorse <andeelou69@outlook.com>
2024-11-25 09:30:17 -0600

    feat api request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+mod groq_api;
+use groq_api::groq_request;
+#[tokio::main]
+
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    groq_request().await?;
+    Ok(())
 }
